655b24b487b12de27ff51e9bd27dfbd0cea60870
liam <liam_killingback@yahoo.com>
2023-03-04 17:58:59 +1100

    changed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { login, register, get_images} from '../controllers/auth.js';
+import { login, register, get_images } from '../controllers/auth.js';
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/register', register);
-router.post('/gallery_images', get_images);
-
+router.get('/gallery_images', get_images);
 
 export default router;
