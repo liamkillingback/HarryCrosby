9f7b4a95ccf20772b6b460f0ff32f3d0b357e0a6
liam <liam_killingback@yahoo.com>
2023-03-07 22:03:21 +1100

    attempt fix

--- server/server.js
+++ server/server.js
@@ -3,18 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import multer from 'multer';
-import util from 'util';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import axios from 'axios';
 import path from 'path';
 import cloudinary from 'cloudinary';
 import { fileURLToPath } from 'url';
-//import authRoutes from './routes/auth.js';
-//import userRoutes from './routes/users.js';
-//import postRoutes from './routes/posts.js';
-import Image from './models/Image.js';
 import authRoutes from './routes/auth.js';
 
 //Configurations
@@ -36,36 +29,11 @@ app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// File Storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/assets');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', "index.html"));
 });
-const upload = multer({ storage });
-
-app.post(
-  '/auth/upload/:genre',
-  upload.array('file', 200),
-  function (req, res, next) {
-    const genre = req.params;
-    console.log(genre.genre);
-    const filename = req.files[0].originalname;
-    const newImage = new Image({
-      imagePath: filename,
-      genre: genre.genre,
-    });
-    newImage.save();
-
-    res.status(201).json({ message: 'pls work bb' });
-  }
-);
 
 // Routes
 app.use('/auth', jsonParser, authRoutes);
